33cc3dbb5ba0f61b7d17779411e7b7d4b237304c
Peepen Sariman <96192069+phisic1714@users.noreply.github.com>
2022-09-15 17:09:46 +0700

    z

---
+++ lib/login.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formstate = GlobalKey<FormState>();
+  String? email;
+  String? password;
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Form(
+      autovalidateMode: AutovalidateMode.always,
+      key: _formstate,
+      child: ListView(
+        children: <Widget>[
+          emailTextFormField(),
+          passwordTextFormField(),
+          loginButton(),
+          registerButton(context),
+        ],
+      ),
+    ));
+  }
+
+  ElevatedButton registerButton(BuildContext context) {
+    return ElevatedButton(
+      // ignore: prefer_const_constructors
+      child: Text('Register new account'),
+      onPressed: () {
+        print('Goto  Regis pagge');
+        Navigator.pushNamed(context, '/register');
+      },
+    );
+  }
+
+  ElevatedButton loginButton() {
+     return ElevatedButton(child: Text('Login'), onPressed: () {});
+  }
+
+  TextFormField passwordTextFormField() {
+    return TextFormField(
+      onSaved: (value) {
+        password = value!.trim();
+      },
+      validator: (value) {
+        if (value!.length < 8)
+          return 'Please Enter more than 8 Character';
+        else
+          return null;
+      },
+      obscureText: true,
+      keyboardType: TextInputType.text,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        icon: Icon(Icons.lock),
+      ),
+    );
+  }
+
+  TextFormField emailTextFormField() {
+    return TextFormField(
+      onSaved: (value) {
+        email = value!.trim();
+      },
+      validator: (value) {
+        if (!validateEmail(value!))
+          return 'Please fill in E-mail field';
+        else
+          return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      decoration: const InputDecoration(
+        labelText: 'E-mail',
+        icon: Icon(Icons.email),
+        hintText: 'x@x.com',
+      ),
+    );
+  }
+
+  bool validateEmail(String value) {
+    RegExp regex = RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+    return (!regex.hasMatch(value)) ? false : true;
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,256 +1,33 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:intl/intl.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:flutter_application_32/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyHome());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
-class MyHome extends StatelessWidget {
-  final appTitle = 'Example Form';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  const MyHome({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return buildMaterialApp();
+  }
+
+  MaterialApp buildMaterialApp() {
     return MaterialApp(
-      home: MyRadio(),
-    );
-  }
-}
-
-class MyRadio extends StatefulWidget {
-  const MyRadio({Key? key}) : super(key: key);
-
-  @override
-  State<MyRadio> createState() => _MyRadioState();
-}
-
-class _MyRadioState extends State<MyRadio> {
-  @override
-  File? _avatar;
-  bool checkboxValueA = true;
-  bool checkboxValueB = true;
-  List<String> provices = ['', 'BKK', 'Pathumthani', 'Outbound'];
-  dynamic provice = '';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        drawer: Drawer(
-          child: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                UserAccountsDrawerHeader(
-                  accountName: Text('พีรพัฒน์ สาริมาน'),
-                  accountEmail: Text('phisic1714@gmail.com'),
-                  currentAccountPicture: CircleAvatar(
-                    child: FlutterLogo(size: 42.0),
-                    backgroundColor: Colors.lightGreen,
-                  ),
-                ),
-                ListTile(
-                  title: Text('Checkbox'),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyRadio()));
-                  },
-                ),
-                ListTile(
-                  title: Text('Radio Button'),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Mycheckbox()));
-                  },
-                )
-              ],
-            ),
-          ),
-        ),
-        body: SingleChildScrollView(child:(Column(children: <Widget>[
-          checkbox(),
-          checkbox_value(),
-          buildSelectField(),
-          buildDateField(),
-          _avatar == null
-              ? ElevatedButton(
-                  onPressed: () {
-                    onChooseImage();
-                  },
-                  child: Text('Choose avatar'),
-                )
-              : Image.file(_avatar!)
-        ]))));
-  }
-
-  onChooseImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        _avatar = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  DateTimeField buildDateField() {
-    final _format = DateFormat('dd/MM/yyyy');
-    return DateTimeField(
-      decoration: InputDecoration(labelText: 'Birth Date'),
-      format: _format,
-      onShowPicker: (context, currentValue) {
-        return showDatePicker(
-            context: context,
-            firstDate: DateTime(1900),
-            initialDate: currentValue ?? DateTime.now(),
-            lastDate: DateTime(2100));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        // '/register': (context) => RegisterPage(),
       },
     );
-  }
-
-  Row checkbox_value() {
-    return Row(children: [
-      Text('$checkboxValueA'),
-      Text('$checkboxValueB'),
-    ]);
-  }
-
-  Row checkbox() {
-    return Row(children: [
-      Checkbox(
-        value: checkboxValueA,
-        onChanged: (bool? value) {
-          setState(() {
-            checkboxValueA = value!;
-          });
-        },
-      ),
-      const Text('CheckBox A'),
-      Checkbox(
-        value: checkboxValueB,
-        onChanged: (bool? value) {
-          setState(() {
-            checkboxValueB = value!;
-          });
-        },
-      ),
-      const Text('CheckBox B'),
-    ]);
-  }
-
-  InputDecorator buildSelectField() {
-    return InputDecorator(
-      decoration: const InputDecoration(labelText: 'Province'),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton(
-          value: provice,
-          onChanged: (value) {
-            setState(() {
-              provice = value;
-            });
-          },
-          items: provices
-              .map(
-                (value) => DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                ),
-              )
-              .toList(),
-        ),
-      ),
-    );
-  }
-}
-
-class Mycheckbox extends StatefulWidget {
-  const Mycheckbox({Key? key}) : super(key: key);
-
-  @override
-  State<Mycheckbox> createState() => _MycheckboxState();
-}
-
-class _MycheckboxState extends State<Mycheckbox> {
-  dynamic route;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo'),
-      ),
-      drawer: Drawer(
-        child: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text('พีรพัฒน์ สาริมาน'),
-                accountEmail: Text('phisic1714@gmail.com'),
-                currentAccountPicture: CircleAvatar(
-                  child: FlutterLogo(size: 42.0),
-                  backgroundColor: Colors.lightGreen,
-                ),
-              ),
-              ListTile(
-                title: Text('Checkbox'),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyRadio()));
-                },
-              ),
-              ListTile(
-                title: Text('Radio Button'),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Mycheckbox()));
-                },
-              )
-            ],
-          ),
-        ),
-      ),
-      body: radio(),
-    );
-  }
-
-  Column radio() {
-    return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(children: [
-            Radio(
-              value: 1,
-              groupValue: route,
-              onChanged: (value) {
-                setState(() {
-                  route = value;
-                });
-              },
-            ),
-            const Text('Round Trip'),
-            Radio(
-              value: 0,
-              groupValue: route,
-              onChanged: (value) {
-                // _handleTapboxChanged(value);
-                setState(() {
-                  route = value;
-                });
-              },
-            ),
-            const Text('One way'),
-          ]),
-          Row(children: [
-            Text('$route'),
-          ]),
-        ]);
   }
 }
