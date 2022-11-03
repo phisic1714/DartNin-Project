ac7c9435033389efe762f545c71fd72f09709e8e
ALEXANDERSSONN <115150753+ALEXANDERSSONN@users.noreply.github.com>
2022-11-04 02:24:09 +0700

    Update register.dart

--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_3/main.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
